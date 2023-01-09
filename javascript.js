403739c9c3d723dab78c918d7f475be0da1aef60
cplummer-mn <c.plummer@everafterdigital.com>
2023-01-08 19:36:37 -0600

    Finished styles and added javascript logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,38 @@
-let passwordOne = document.querySelector('#password');
+const passwordOne = document.querySelector('#password');
+const passwordTwo = document.querySelector('#password-confirm');
+
+
+const passwordInfoText = document.querySelector('#password-info-text');
+
 
 function showPass() {
-    return passwordOne.value;
+    return passwordOne.value + passwordTwo.value;
 }
+
+
+function checkPass() {
+    if(passwordOne.value !== "" && passwordTwo.value !== "") {
+        if(passwordOne.value !== passwordTwo.value) {
+            passwordInfoText.textContent = 'PASSWORDS DO NOT MATCH'
+        }
+    }
+}
+
+passwordTwo.addEventListener('input', () => {
+    if(passwordOne.value !== "" && passwordTwo.value !== "") {
+        if(passwordOne.value == passwordTwo.value) {
+            passwordInfoText.style.display = 'none'
+            passwordTwo.style.border = '1px solid #D6D9DC';
+            passwordTwo.style.color =  'black';
+        }
+        else {
+            passwordInfoText.style.display = 'block'
+            passwordTwo.style.border = '3px solid #ff0004';
+            passwordTwo.style.color =  '#D55C5F';
+            passwordInfoText.textContent = 'PASSWORDS DO NOT MATCH'
+        }
+
+    }
+})
+
+
